691020016aa320ae0a06ebdfa1f276af7ec17c0c
chonchon0516 <azumi.toyoda@gmail.com>
2022-11-08 22:30:03 +0900

    CSS[基礎編初級①]課題6

--- src/public/practiceWordColor/q5.php
+++ src/public/practiceWordColor/q5.php
@@ -2,11 +2,12 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/practoceWordColor/q5.css">
   <title>Document</title>
 </head>
 
 <h1>h1タグ</h1>
 <h2>h2タグ</h2>
 <h3>h3タグ</h3>
-<span>spanタグ1</span>
-<span>spanタグ2</span>
+<span class ="spanTag1">spanタグ1</span>
+<span class ="spanTag2">spanタグ2</span>
