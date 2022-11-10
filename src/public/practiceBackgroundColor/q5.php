335d1b8a1675689bec6e75b0c2ae4681895dda26
chonchon0516 <azumi.toyoda@gmail.com>
2022-11-10 22:33:52 +0900

    CSS[基礎編初級②]課題5

--- src/public/practiceBackgroundColor/q5.php
+++ src/public/practiceBackgroundColor/q5.php
@@ -5,12 +5,18 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/practiceBackgroundColor/q5.css">
   <title>Document</title>
 </head>
 
 <body>
-  <main>
-    
+  <main class="main-upper">
+    <span class="main__text--white">内容1</span>
+    <span class="main__text--orange">内容2</span>
+  </main>
+  <main class="main-down">
+    <span class="main__text--yellow">内容3</span>
+    <span class="main__text--white">内容4</span>
   </main>
 </body>
 
