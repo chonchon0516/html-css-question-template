2204f38d98189e06f107970f30c0a1baf23f015a
chonchon0516 <azumi.toyoda@gmail.com>
2022-11-18 11:05:13 +0900

    CSS[基礎編中級②]課題6

--- src/public/practiceFlexBeginner/q5.php
+++ src/public/practiceFlexBeginner/q5.php
@@ -4,12 +4,39 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="stylesheet" href="../css/practiceFlexBeginner/q5.css">
+
   <title>Document</title>
 </head>
 
-<body>
-  
+<body class="body">
+  <main class="main">
+
+    <div class="buttonsWrapper">
+      
+      <div class="buttonWrapper">
+        <button class="button">ボタン1</button>
+      </div>
+      
+      <div class="buttonWrapper">
+        <button class="button">ボタン2</button>
+      </div>
+      
+      <div class="buttonWrapper">
+        <button class="button">ボタン3</button>
+      </div>
+
+      <div class="buttonWrapper">
+        <button class="button">ボタン4</button>
+      </div>
+
+      <div class="buttonWrapper">
+        <button class="button">ボタン5</button>
+      </div>
+      
+    </div>
+
+  </main>
 </body>
 
 </html>
