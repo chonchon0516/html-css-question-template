40970fafd8d4a958083cb26eb4f3410d89dc9de7
chonchon0516 <azumi.toyoda@gmail.com>
2022-11-24 21:41:30 +0900

    CSS[基礎編中級③]課題3

--- src/public/practiceFlexIntermediate/q3.php
+++ src/public/practiceFlexIntermediate/q3.php
@@ -5,11 +5,62 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/practiceFlexIntermediate/q3.css">
   <title>Document</title>
 </head>
 
-<body>
-  
+<body class="body">
+  <main class="main">
+
+    <div class="topButtonsWrapper">
+      
+      <div class="buttonWrapper">
+        <button class="button">ボタン1</button>
+      </div>
+      
+      <div class="buttonWrapper">
+        <button class="button">ボタン2</button>
+      </div>
+
+      <div class="buttonWrapper">
+        <button class="button">ボタン3</button>
+      </div>
+      
+    </div>
+
+    <div class="middleButtonsWrapper">
+      
+      <div class="buttonWrapper">
+        <button class="button">ボタン1</button>
+      </div>
+      
+      <div class="buttonWrapper">
+        <button class="button">ボタン2</button>
+      </div>
+
+       <div class="buttonWrapper">
+        <button class="button">ボタン3</button>
+      </div>
+      
+    </div>
+
+    <div class="bottomButtonsWrapper">
+      
+      <div class="buttonWrapper">
+        <button class="button">ボタン1</button>
+      </div>
+      
+      <div class="buttonWrapper">
+        <button class="button">ボタン2</button>
+      </div>
+
+       <div class="buttonWrapper">
+        <button class="button">ボタン3</button>
+      </div>
+      
+    </div>
+
+  </main>
 </body>
 
 </html>
