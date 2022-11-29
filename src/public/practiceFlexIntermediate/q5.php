92c370931d1ce283dca8b7fcd6bcca0cfde2493a
chonchon0516 <azumi.toyoda@gmail.com>
2022-11-29 21:15:16 +0900

    CSS[基礎編中級③]課題5ｰ1

---
+++ src/public/practiceFlexIntermediate/q5.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/practiceFlexIntermediate/q5.css">
+  <title>Document</title>
+</head>
+
+<body class="body">
+  <main class="main">
+
+    <div class="topButton">
+          
+      <div class="buttonWrapper0">
+        <button class="button">レフトボタン</button>
+    </div>
+
+    <div> 
+      <div class="buttonWrapper">
+          <button class="button">ライトボタン1</button>
+        </div>
+          
+        <div class="bottomButtonsWrapper"> 
+      
+          <div>
+            <button class="button">ライトボタン２</button>
+          </div>
+
+          <div>
+            <button class="button">ライトボタン３</button>
+          </div>
+        </div> 
+    </div>
+  </div>
+ </main>
+</body>
+
+</html>
